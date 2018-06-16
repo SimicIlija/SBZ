fbd44d7fe54d88fbd1678bf83ad7f46a1686a8c0
SimicIlija <simicsilija@gmail.com>
2018-06-16 22:59:15 +0200

    Diagnose controller

--- drools-spring-v2-app/src/main/java/drools/spring/example/controller/DiagnoseController.java
+++ drools-spring-v2-app/src/main/java/drools/spring/example/controller/DiagnoseController.java
@@ -1,14 +1,16 @@
 package drools.spring.example.controller;
 
+import drools.spring.example.model.Disease;
+import drools.spring.example.model.dto.DiseaseDto;
 import drools.spring.example.model.dto.SymptomsDto;
 import drools.spring.example.service.DiagnoseService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/api/diagnose")
@@ -23,8 +25,31 @@ public class DiagnoseController {
     @RequestMapping(method = RequestMethod.POST, value = "/{userId}",
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> getDiseases(@PathVariable Long userId, SymptomsDto symptomsDto){
-
-        return null;
+    public ResponseEntity<?> getDiseases(@PathVariable Long userId, @RequestBody SymptomsDto symptomsDto) {
+        List<Disease> diseases = diagnoseService.getPossibleDis(userId, symptomsDto);
+        return new ResponseEntity<Object>(diseases, HttpStatus.OK);
     }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/connected/{userId}",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> getConnected(@PathVariable Long userId, @RequestBody SymptomsDto symptomsDto) {
+        List<DiseaseDto> diseases = diagnoseService.getConnected(userId, symptomsDto);
+        return new ResponseEntity<Object>(diseases, HttpStatus.OK);
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/bolesti/{diseaseId}",
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> getSymptoms(@PathVariable Long diseaseId) {
+        SymptomsDto dto = diagnoseService.findSymptoms(diseaseId);
+        return new ResponseEntity<Object>(dto, HttpStatus.OK);
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/drug/{drugId}/patient/{patientId}",
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> drugValidate(@PathVariable Long drugId, @PathVariable Long patientId) {
+        boolean result = diagnoseService.validate(drugId, patientId);
+        return new ResponseEntity<Object>(result, HttpStatus.OK);
+    }
+
 }
--- drools-spring-v2-app/src/main/java/drools/spring/example/service/DiagnoseService.java
+++ drools-spring-v2-app/src/main/java/drools/spring/example/service/DiagnoseService.java
@@ -1,7 +1,98 @@
 package drools.spring.example.service;
 
+import drools.spring.example.controller.exception.NotFoundException;
+import drools.spring.example.model.*;
+import drools.spring.example.model.dto.DiseaseDto;
+import drools.spring.example.model.dto.SymptomsDto;
+import drools.spring.example.repository.DiseaseRepository;
+import drools.spring.example.repository.DrugRepository;
+import drools.spring.example.repository.PatientRepository;
+import drools.spring.example.repository.VisitRepository;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class DiagnoseService {
+
+    private final PatientRepository patientRepository;
+    private final VisitRepository visitRepository;
+    private final DiseaseRepository diseaseRepository;
+    private final DrugRepository drugRepository;
+    private final KieContainer kieContainer;
+
+    @Autowired
+    public DiagnoseService(PatientRepository patientRepository, VisitRepository visitRepository, DiseaseRepository diseaseRepository, DrugRepository drugRepository, KieContainer kieContainer) {
+        this.patientRepository = patientRepository;
+        this.visitRepository = visitRepository;
+        this.diseaseRepository = diseaseRepository;
+        this.drugRepository = drugRepository;
+        this.kieContainer = kieContainer;
+    }
+
+    public List<Disease> getPossibleDis(Long userId, SymptomsDto symptomsDto) {
+        Patient patient = patientRepository.findById(userId).orElseThrow(() -> new NotFoundException("ne postoji pacijent"));
+        List<Visit> previousVisits = visitRepository.findByPatient_Id(patient.getId());
+        ReasonInput input = new ReasonInput();
+        input.setSymptoms(symptomsDto.getSymptoms());
+        input.setPreviousVisits(previousVisits);
+        KieSession kieSession = kieContainer.newKieSession();
+        kieSession.insert(input);
+        kieSession.getAgenda().getAgendaGroup("simptomi").setFocus();
+        kieSession.fireAllRules();
+        kieSession.insert(input);
+        kieSession.getAgenda().getAgendaGroup("diagnose").setFocus();
+        kieSession.fireAllRules();
+        kieSession.dispose();
+        List<Disease> retVal = new ArrayList<>();
+        retVal.add(input.getFirstDisease());
+        retVal.add(input.getThirdDisease());
+        retVal.addAll(input.getSecondDiseases());
+        return retVal;
+    }
+
+    public List<DiseaseDto> getConnected(Long userId, SymptomsDto symptomsDto) {
+        Patient patient = patientRepository.findById(userId).orElseThrow(() -> new NotFoundException("ne postoji pacijent"));
+        List<Visit> previousVisits = visitRepository.findByPatient_Id(patient.getId());
+        ReasonInput input = new ReasonInput();
+        input.setSymptoms(symptomsDto.getSymptoms());
+        input.setPreviousVisits(previousVisits);
+        KieSession kieSession = kieContainer.newKieSession();
+        kieSession.insert(input);
+        kieSession.getAgenda().getAgendaGroup("simptomi").setFocus();
+        kieSession.fireAllRules();
+        kieSession.insert(input);
+        kieSession.getAgenda().getAgendaGroup("connected").setFocus();
+        kieSession.fireAllRules();
+        kieSession.dispose();
+        return input.getConnectedDisease();
+    }
+
+    public SymptomsDto findSymptoms(Long diseaseId) {
+        Disease disease = diseaseRepository.findById(diseaseId).orElseThrow(() -> new NotFoundException("nije nadjena bolest"));
+        DiseaseInput input = new DiseaseInput();
+        input.setDisease(disease);
+        KieSession kieSession = kieContainer.newKieSession();
+        kieSession.insert(input);
+        kieSession.getAgenda().getAgendaGroup("bolesti").setFocus();
+        kieSession.fireAllRules();
+        kieSession.dispose();
+        return input.getSymptoms();
+    }
+
+    public boolean validate(Long drugId, Long patientId) {
+        Patient patient = patientRepository.findById(patientId).orElseThrow(() -> new NotFoundException("Ne postoji pacijent"));
+        Drug drug = drugRepository.findById(drugId).orElseThrow(() -> new NotFoundException("Nema leka"));
+        DrugValidationInput input = new DrugValidationInput(drug.getIngredientList(), patient.getAllergies());
+        KieSession kieSession = kieContainer.newKieSession();
+        kieSession.insert(input);
+        kieSession.getAgenda().getAgendaGroup("drug").setFocus();
+        kieSession.fireAllRules();
+        kieSession.dispose();
+        return input.isResult();
+    }
 }
